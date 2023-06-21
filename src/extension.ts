77cd773d2d33bb79ac2ef38d8388d43ac0f25680
Jake Demian <jakedemian@gmail.com>
2023-06-20 21:19:28 -0400

    format on save

--- src/extension.ts
+++ src/extension.ts
@@ -4,42 +4,60 @@ import * as path from 'path';
 import { Minimatch } from 'minimatch';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('project-hierarchy-explorer.generate', () => {
-        const filePath = path.join(vscode.workspace.rootPath!, 'project-hierarchy.txt');
-        const hierarchy = getProjectName() + '\n' + getDirectoryStructure(vscode.workspace.rootPath!);
-        fs.writeFileSync(filePath, hierarchy);
-        vscode.window.showInformationMessage('Success! Check project-hierarchy.txt in the root of your project');
-    });
+  let disposable = vscode.commands.registerCommand(
+    'project-hierarchy-explorer.generate',
+    () => {
+      const filePath = path.join(
+        vscode.workspace.rootPath!,
+        'project-hierarchy.txt'
+      );
+      const hierarchy =
+        getProjectName() +
+        '\n' +
+        getDirectoryStructure(vscode.workspace.rootPath!);
+      fs.writeFileSync(filePath, hierarchy);
+      vscode.window.showInformationMessage(
+        'Success! Check project-hierarchy.txt in the root of your project'
+      );
+    }
+  );
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 function getProjectName(): string {
-    const dir = fs.readdirSync(vscode.workspace.rootPath!);
-    const packageJson = dir.find((file) => file === 'package.json');
-    if (packageJson) {
-        const packageJsonPath = path.join(vscode.workspace.rootPath!, packageJson);
-        const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
-        const packageJsonParsed = JSON.parse(packageJsonContent);
-        return packageJsonParsed.name;
-    }
-    return "Project";
+  const dir = fs.readdirSync(vscode.workspace.rootPath!);
+  const packageJson = dir.find(file => file === 'package.json');
+  if (packageJson) {
+    const packageJsonPath = path.join(vscode.workspace.rootPath!, packageJson);
+    const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+    const packageJsonParsed = JSON.parse(packageJsonContent);
+    return packageJsonParsed.name;
+  }
+  return 'Project';
 }
 
 function getDirectoryStructure(dirPath: string, prefix = ''): string {
-    const dir = fs.readdirSync(dirPath);
-    const ignorePatterns = vscode.workspace.getConfiguration('project-hierarchy-explorer').get<string[]>('ignorePatterns') || [];
-    let structure = '';
-    const filteredDir = dir.filter(file => !ignorePatterns.some(pattern => new Minimatch(pattern).match(file)));
-    filteredDir.forEach((file, index, array) => {
-        const filePath = path.join(dirPath, file);
-        const isDir = fs.statSync(filePath).isDirectory();
-        const isLast = index === array.length - 1;
-        structure += prefix + (isLast ? '└─ ' : '├─ ') + file + '\n';
-        if (isDir) {
-            structure += getDirectoryStructure(filePath, isLast ? prefix + '   ' : prefix + '│  ');
-        }
-    });
-    return structure;
+  const dir = fs.readdirSync(dirPath);
+  const ignorePatterns =
+    vscode.workspace
+      .getConfiguration('project-hierarchy-explorer')
+      .get<string[]>('ignorePatterns') || [];
+  let structure = '';
+  const filteredDir = dir.filter(
+    file => !ignorePatterns.some(pattern => new Minimatch(pattern).match(file))
+  );
+  filteredDir.forEach((file, index, array) => {
+    const filePath = path.join(dirPath, file);
+    const isDir = fs.statSync(filePath).isDirectory();
+    const isLast = index === array.length - 1;
+    structure += prefix + (isLast ? '└─ ' : '├─ ') + file + '\n';
+    if (isDir) {
+      structure += getDirectoryStructure(
+        filePath,
+        isLast ? prefix + '   ' : prefix + '│  '
+      );
+    }
+  });
+  return structure;
 }
-
