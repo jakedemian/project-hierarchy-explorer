16c7c967e7a4674b49e9ad4ca0da11577dede7ad
Jake Demian <jakedemian@gmail.com>
2023-07-13 15:34:17 -0400

    generate function logic modularized

---
+++ src/commands/generate.ts
@@ -0,0 +1,47 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import { getRootPath } from '../utils/getRootPath';
+import { getDirectoryStructure } from '../utils/getDirectoryStructure';
+import { getConfiguration } from '../utils/getConfiguration';
+import { OUTPUT_FILE_NAME, SUCCESS_MESSAGE } from '../extension';
+
+export async function generate() {
+  const rootPath = getRootPath();
+
+  if (!rootPath) {
+    vscode.window.showErrorMessage('No root path found');
+    return;
+  }
+
+  const ignorePatterns: string[] = getConfiguration('ignorePatterns') ?? [];
+  const hierarchy = await getDirectoryStructure(rootPath, ignorePatterns);
+  const result = path.basename(rootPath) + '\n' + hierarchy;
+
+  const outputsTo: string = getConfiguration('outputsTo') ?? 'file';
+
+  if (outputsTo === 'file' || outputsTo === 'both') {
+    const outputFilePath = path.join(rootPath, OUTPUT_FILE_NAME);
+    fs.writeFileSync(outputFilePath, result);
+
+    vscode.workspace.openTextDocument(outputFilePath).then(doc => {
+      vscode.window.showTextDocument(doc);
+    });
+  }
+
+  if (outputsTo === 'console' || outputsTo === 'both') {
+    const outputChannel = vscode.window.createOutputChannel(
+      'Project Hierarchy Explorer'
+    );
+    outputChannel.append(result);
+    outputChannel.show();
+  }
+
+  let suppressNotification: boolean =
+    getConfiguration('suppressNotification') ?? true;
+
+  if (!suppressNotification) {
+    vscode.window.showInformationMessage(SUCCESS_MESSAGE);
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,5 @@
 import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-
-import { getRootPath } from './utils/getRootPath';
-import { getDirectoryStructure } from './utils/getDirectoryStructure';
-import { getConfiguration } from './utils/getConfiguration';
+import { generate } from './commands/generate';
 
 export const OUTPUT_FILE_NAME = 'project-hierarchy.txt';
 export const SUCCESS_MESSAGE = 'Success!';
@@ -12,44 +7,7 @@ export const SUCCESS_MESSAGE = 'Success!';
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     'project-hierarchy-explorer.generate',
-    async () => {
-      const rootPath = getRootPath();
-
-      if (!rootPath) {
-        vscode.window.showErrorMessage('No root path found');
-        return;
-      }
-
-      const ignorePatterns: string[] = getConfiguration('ignorePatterns') ?? [];
-      const hierarchy = await getDirectoryStructure(rootPath, ignorePatterns);
-      const result = path.basename(rootPath) + '\n' + hierarchy;
-
-      const outputsTo: string = getConfiguration('outputsTo') ?? 'file';
-
-      if (outputsTo === 'file' || outputsTo === 'both') {
-        const outputFilePath = path.join(rootPath, OUTPUT_FILE_NAME);
-        fs.writeFileSync(outputFilePath, result);
-
-        vscode.workspace.openTextDocument(outputFilePath).then(doc => {
-          vscode.window.showTextDocument(doc);
-        });
-      }
-
-      if (outputsTo === 'console' || outputsTo === 'both') {
-        const outputChannel = vscode.window.createOutputChannel(
-          'Project Hierarchy Explorer'
-        );
-        outputChannel.append(result);
-        outputChannel.show();
-      }
-
-      let suppressNotification: boolean =
-        getConfiguration('suppressNotification') ?? true;
-
-      if (!suppressNotification) {
-        vscode.window.showInformationMessage(SUCCESS_MESSAGE);
-      }
-    }
+    async () => generate()
   );
 
   context.subscriptions.push(disposable);
