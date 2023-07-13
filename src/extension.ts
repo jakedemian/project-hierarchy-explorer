deab45d87f094b309f2fe95963051e05351344b9
Jake Demian <jakedemian@gmail.com>
2023-07-13 15:41:51 -0400

    registered generatePartial command

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,20 @@
 import * as vscode from 'vscode';
 import { generate } from './commands/generate';
+import { generatePartial } from './commands/generatePartial';
 
 export const OUTPUT_FILE_NAME = 'project-hierarchy.txt';
 export const SUCCESS_MESSAGE = 'Success!';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
+  let _generate = vscode.commands.registerCommand(
     'project-hierarchy-explorer.generate',
     async () => generate()
   );
 
-  context.subscriptions.push(disposable);
+  let _generatePartial = vscode.commands.registerCommand(
+    'project-hierarchy-explorer.generatePartial',
+    async () => generatePartial()
+  );
+
+  context.subscriptions.push(_generate, _generatePartial);
 }
